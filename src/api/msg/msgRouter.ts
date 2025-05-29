fba51b6c23526cd4cbca260121470788c6d68fa7
Daniel Wilson <danwilsooon@gmail.com>
2025-05-29 12:03:22 -0400

    testing reply

--- src/api/msg/msgRouter.ts
+++ src/api/msg/msgRouter.ts
@@ -1,8 +1,14 @@
 import express, { type Router } from "express"
+import MessagingResponse from "twilio/lib/twiml/MessagingResponse"
 
 export const msgRouter: Router = express.Router()
 
-msgRouter.post("/msg", (request) => {
+msgRouter.post("/msg", (request, response) => {
   const { Body: stopNumber } = request.body
   console.log("Received stop number:", stopNumber)
+  const twiml = new MessagingResponse()
+
+  twiml.message("The Robots are coming! Head for the hills!")
+
+  response.type("text/xml").send(twiml.toString())
 })
