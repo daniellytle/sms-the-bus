476933760beb5b58a42890bcd2e56595dfacc31e
Daniel Wilson <danwilsooon@gmail.com>
2025-05-29 12:00:00 -0400

    testing

--- src/api/msg/msgRouter.ts
+++ src/api/msg/msgRouter.ts
@@ -3,5 +3,6 @@ import express, { type Router } from "express"
 export const msgRouter: Router = express.Router()
 
 msgRouter.post("/msg", (request) => {
-  console.log("Received message:", request.body)
+  const { Body: stopNumber } = request.body
+  console.log("Received stop number:", stopNumber)
 })
